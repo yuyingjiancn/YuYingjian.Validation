c8592c469b8df399c2f7885fc25ee6208b29aa42
yuyingjiancn <yuyingjian@gmail.com>
2015-02-08 13:23:45 +0800

    0.0.2

--- YuYingjian.Validation/ValidationContext.cs
+++ YuYingjian.Validation/ValidationContext.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace YuYingjian.Validation
 {
+    /// <summary>
+    /// 验证上下文类
+    /// </summary>
     public class ValidationContext
     {
+        /// <summary>
+        /// 验证是否通过
+        /// </summary>
         public bool IsValid { get; set; }
+
+        /// <summary>
+        /// 存储验证的值，如果是值类型如int、double则验证通过后会自动转换为相应类型
+        /// </summary>
         public object Value { get; set; }
+
+        /// <summary>
+        /// 验证未通过的错误信息
+        /// </summary>
         public string Message { get; set; }
     }
 }
