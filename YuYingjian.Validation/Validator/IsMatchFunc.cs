4010fd84342a24c26cee2d7177a499437e0618c7
yuyingjiancn <yuyingjian@gmail.com>
2015-02-08 19:08:25 +0800

    添加自定义方法验证

---
+++ YuYingjian.Validation/Validator/IsMatchFunc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YuYingjian.Validation.Validator
+{
+    public static partial class Validator
+    {
+        /// <summary>
+        /// 判断是否匹配方法验证结果
+        /// </summary>
+        /// <param name="v"></param>
+        /// <param name="matchFunc"></param>
+        /// <returns></returns>
+        public static ValidationContext IsMatchFunc(this object v, Func<object, bool> matchFunc )
+        {
+            bool state = matchFunc.Invoke(v);
+
+            return new ValidationContext
+            {
+                IsValid = state,
+                Value = v,
+                Message = "无法通过方法验证"
+            };
+        }
+
+        public static ValidationContext IsMatchFunc(this ValidationContext vc, Func<object, bool> matchFunc)
+        {
+            if (!vc.IsValid) return vc;
+            return vc.Value.IsMatchFunc(matchFunc);
+        }
+    }
+}
